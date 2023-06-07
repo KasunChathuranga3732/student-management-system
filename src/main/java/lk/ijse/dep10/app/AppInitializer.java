c989db4e9d4c1dcdbbcc9c5ad1b936072305778d
kasun-chathuranga <kasunchathuranga3732@gmail.com>
2023-06-07 10:20:49 +0530

    Add methods to create database and tables

--- src/main/java/lk/ijse/dep10/app/AppInitializer.java
+++ src/main/java/lk/ijse/dep10/app/AppInitializer.java
@@ -3,10 +3,21 @@ package lk.ijse.dep10.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import lk.ijse.dep10.app.db.DBConnection;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashSet;
+import java.util.Set;
 
 public class AppInitializer extends Application {
 
@@ -17,6 +28,8 @@ public class AppInitializer extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        generateTables();
+
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/ManageStudentView.fxml"));
         AnchorPane root = fxmlLoader.load();
         primaryStage.setScene(new Scene(root));
@@ -24,6 +37,47 @@ public class AppInitializer extends Application {
         primaryStage.setTitle("Manage Students");
         primaryStage.show();
         primaryStage.centerOnScreen();
+    }
+
+    private void generateTables() {
+        Connection connection = DBConnection.getInstance().getConnection();
+
+        try {
+            Statement stm = connection.createStatement();
+
+            ResultSet rst = stm.executeQuery("SHOW TABLES ");
+            HashSet<String> tableSet = new HashSet<>();
+
+            while(rst.next()){
+                tableSet.add(rst.getString(1));
+            }
+
+            boolean tableExists = tableSet.containsAll(Set.of("Picture", "Student"));
+
+            if(!tableExists) stm.execute(readDBScript());
+
+        } catch (SQLException e) {
+            new Alert(Alert.AlertType.ERROR, "Failed to create tables").showAndWait();
+            e.printStackTrace();
+        }
+    }
+
+    private String readDBScript(){
+        try {
+            InputStream is = getClass().getResourceAsStream("/schema.sql");
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuilder dbScript = new StringBuilder();
+
+            while ((line = br.readLine()) != null) {
+                dbScript.append(line).append("\n");
+            }
+            br.close();
+            return dbScript.toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
